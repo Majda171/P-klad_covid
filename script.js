261f7f3a871c32c1e79e9a3b0d23261bcc92fb10
Magdaléna Škobrtalová <mahdalova9@seznam.cz>
2024-10-07 19:11:20 +0200

    Příklad první i druhý

---
+++ script.js
@@ -0,0 +1,10 @@
+//const name = prompt("Jak se jmenujete?");
+//const age = Number(prompt("Zadejte svůj věk."));
+
+//document.body.innerHTML = `<p> ${name}, věk: ${age}`;
+const pocetDnu = 21;
+const pocetHodin = 8;
+let hodinovaMzda = Number(prompt("Zadej hodinovou mzdu."));
+const mzda = (pocetHodin * hodinovaMzda) * pocetDnu;
+document.body.innerHTML = `<p>Měsíční hrubá mzda je ${mzda}</p>`;
+
